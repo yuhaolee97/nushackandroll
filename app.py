0f53e136436a42587f74fc692d3bd9d49873f088
Yu Hao Lee <yuhaolee@Yus-MacBook-Pro.local>
2021-01-09 11:27:43 +0800

    Final file front/back end

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def my_form_post():
         counter += 1
 
     # print(distance_dictionary)
-    top_4_dictionaries = collections.Counter(distance_dictionary).most_common()[:-5:-1]
+    top_4_dictionaries = collections.Counter(distance_dictionary).most_common()[:-3:-1]
 
 
     top_4_dictionaries = dict(top_4_dictionaries)
@@ -145,45 +145,45 @@ def my_form_post():
     top_10_dictionaries = {}
     crowdlevelfiles = []
     for row, col in filtered_df.iterrows(): #to see which crowd level is the lowest
-        # params = {
-        #     'api_key_private': 'pri_4b5d2bc1abb749d49af70ad45ab9274a',
-        #     'venue_name': filtered_df['Restaurant'][row],
-        #     'venue_address': filtered_df['Address'][row]
-        # }
+        params = {
+            'api_key_private': 'pri_4b5d2bc1abb749d49af70ad45ab9274a',
+            'venue_name': filtered_df['Restaurant'][row],
+            'venue_address': filtered_df['Address'][row]
+        }
 
-        # response = requests.request("POST", url, params=params)
+        response = requests.request("POST", url, params=params)
 
-        # data = json.loads(response.text)
+        data = json.loads(response.text)
 
 
-        # hours = ["6AM","7AM","8AM", "9AM","10AM","11AM", "12PM","1PM","2PM", "3PM","4PM","5PM", "6PM","7PM","8PM","9PM","10PM","11PM", "12AM"]
-        # if (data):
-        #     print(data['analysis'])
-        #     percent_fullness = data['analysis'][5]['day_raw'][:-5]
-        #     intensity = data['analysis'][5]['day_info']['day_rank_max']
-        #     intensity = 8 - intensity
-        #     plotdata = [go.Bar(x = hours, y = percent_fullness)]
-        #     fig = go.Figure(data=plotdata)
-        #     fig.update_yaxes(range=[0, 100], color='lightgrey', showgrid=False)
-        #     fig.update_layout(
-        #         autosize=False,
-        #         width=800,
-        #         height=400,
-        #         paper_bgcolor='rgba(0,0,0,0)',
-        #         plot_bgcolor='rgba(0,0,0,0)'
-        #     )
-        #     fig.update_layout(
-        #         xaxis = dict(
-        #             tickmode = 'array',
-        #             tickvals = ["9AM", "12PM", "3PM","6PM","9PM", "12AM"], showgrid=False, color='lightgrey'
-        #         )
-        #     )
-        #     # fig.show()
-        #     fig.write_image("./static/images/crowdlevel" + str(counter) + ".png")
-        #     crowdlevel_lists.append('images/crowdlevel' + str(counter) + ".png")
+        hours = ["6AM","7AM","8AM", "9AM","10AM","11AM", "12PM","1PM","2PM", "3PM","4PM","5PM", "6PM","7PM","8PM","9PM","10PM","11PM", "12AM"]
+        if (data):
+            print(data['analysis'])
+            percent_fullness = data['analysis'][5]['day_raw'][:-5]
+            intensity = data['analysis'][5]['day_info']['day_rank_max']
+            intensity = 8 - intensity
+            plotdata = [go.Bar(x = hours, y = percent_fullness)]
+            fig = go.Figure(data=plotdata)
+            fig.update_yaxes(range=[0, 100], color='lightgrey', showgrid=False)
+            fig.update_layout(
+                autosize=False,
+                width=800,
+                height=400,
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0,0)'
+            )
+            fig.update_layout(
+                xaxis = dict(
+                    tickmode = 'array',
+                    tickvals = ["9AM", "12PM", "3PM","6PM","9PM", "12AM"], showgrid=False, color='lightgrey'
+                )
+            )
+            # fig.show()
+            fig.write_image("./static/images/crowdlevel" + str(counter) + ".png")
+            crowdlevel_lists.append('images/crowdlevel' + str(counter) + ".png")
         intensity = 4
         sorted_crowd_dictionary[row] = intensity
-        if (counter == 3): 
+        if (counter == 5): 
             break
         crowdlevelfiles.append('images/graph' + str(counter) + ".png")
         counter += 1
@@ -216,7 +216,7 @@ def my_form_post():
         map_titles.append(filtered_df['Google_map_link'][row])
         cusine_lists.append(filtered_df['Cusines'][row])
 
-        if (counter == 3):
+        if (counter == 5):
             break
 
         counter += 1

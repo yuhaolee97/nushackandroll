ce4d281a69fde4fc3fb460b1e4cfe32cd12da570
Yu Hao Lee <yuhaolee@Yus-MacBook-Pro.local>
2021-01-09 09:51:22 +0800

    Push

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def my_form_post():
         cluster_dictionary[Coordinates(x, y)] = km.labels_[counter]
         counter += 1
 
-    print(distance_dictionary)
+    # print(distance_dictionary)
     top_4_dictionaries = collections.Counter(distance_dictionary).most_common()[:-5:-1]
 
 
@@ -115,7 +115,7 @@ def my_form_post():
             origins.append(str(key.latitude) + ' ' + str(key.longitude))
             destinations.append(str(j[0]) + ' ' + str(j[1]))
 
-            matrix = gmaps.distance_matrix(origins, destinations, mode = "transit")
+            matrix = gmaps.distance_matrix(origins, destinations)
             print(matrix)
 
             sums += matrix['rows'][0]['elements'][0]['distance']['value']
@@ -129,7 +129,6 @@ def my_form_post():
     filename = "./images"
 
     filtered_df = latlong_df.loc[latlong_df['Clusters'] == centroid]
-    # print(filtered_df.head())
 
     filename_lists = []
     map_titles = []
@@ -138,8 +137,59 @@ def my_form_post():
     cusine_lists = []
     crowdlevel_lists = []
 
+    url = "https://besttime.app/api/v1/forecasts"
+
     counter = 0
-    for row, col in filtered_df.iterrows():
+
+    sorted_crowd_dictionary = {}
+    top_10_dictionaries = {}
+    # for row, col in filtered_df.iterrows(): #to see which crowd level is the lowest
+    #     params = {
+    #         'api_key_private': 'pri_4b5d2bc1abb749d49af70ad45ab9274a',
+    #         'venue_name': filtered_df['Restaurant'][row],
+    #         'venue_address': filtered_df['Address'][row]
+    #     }
+
+    #     response = requests.request("POST", url, params=params)
+
+    #     data = json.loads(response.text)
+
+
+    #     hours = ["6AM","7AM","8AM", "9AM","10AM","11AM", "12PM","1PM","2PM", "3PM","4PM","5PM", "6PM","7PM","8PM","9PM","10PM","11PM", "12AM"]
+    #     if (data):
+    #         print(data['analysis'])
+    #         percent_fullness = data['analysis'][5]['day_raw'][:-5]
+    #         intensity = data['analysis'][5]['day_info']['day_rank_max']
+    #         intensity = 8 - intensity
+    #         plotdata = [go.Bar(x = hours, y = percent_fullness)]
+    #         fig = go.Figure(data=plotdata)
+    #         fig.update_yaxes(range=[0, 100], color='lightgrey', showgrid=False)
+    #         fig.update_layout(
+    #             autosize=False,
+    #             width=800,
+    #             height=400,
+    #             paper_bgcolor='rgba(0,0,0,0)',
+    #             plot_bgcolor='rgba(0,0,0,0)'
+    #         )
+    #         fig.update_layout(
+    #             xaxis = dict(
+    #                 tickmode = 'array',
+    #                 tickvals = ["9AM", "12PM", "3PM","6PM","9PM", "12AM"], showgrid=False, color='lightgrey'
+    #             )
+    #         )
+    #         # fig.show()
+    #         fig.write_image("./static/images/crowdlevel" + str(counter) + ".png")
+    #         crowdlevel_lists.append('images/crowdlevel' + str(counter) + ".png")
+    #         sorted_crowd_dictionary[row] = intensity
+    #     if (counter == 5): 
+    #         break
+    #     counter += 1
+
+    # top_10_dictionaries = collections.Counter(sorted_crowd_dictionary).most_common(counter)
+    # top_10_dictionaries = dict(top_10_dictionaries)
+
+    counter = 0
+    for row in top_10_dictionaries:
         photo_reference = filtered_df['Photo_reference'][row]
 
         print(photo_reference)
@@ -149,57 +199,22 @@ def my_form_post():
 
         raw_image_data = gmaps.places_photo(photo_reference = photo_reference, max_width = 1600) #400 - 1600 (max)
         
-        imgfile = open('./static/images/myImage' + str(counter) + '.jpg', 'wb')
+        imgfile = open('./static/images/myImage' + str(row) + '.jpg', 'wb')
 
         for chunk in raw_image_data:
             if chunk:
                 imgfile.write(chunk)
         imgfile.close()
-        filename_lists.append('images/myImage' + str(counter) + '.jpg')
+        filename_lists.append('images/myImage' + str(row) + '.jpg')
 
         restaurant_lists.append(filtered_df['Restaurant'][row])
         address_lists.append(filtered_df['Address'][row])
         map_titles.append(filtered_df['Google_map_link'][row])
         cusine_lists.append(filtered_df['Cusines'][row])
 
-        # url = "https://besttime.app/api/v1/forecasts"
-
-        # params = {
-        #     'api_key_private': 'pri_db35aee5b0a14d168d2c32a2f2e8891d',
-        #     'venue_name': filtered_df['Restaurant'][row],
-        #     'venue_address': filtered_df['Address'][row]
-        # }
-
-        # response = requests.request("POST", url, params=params)
-
-        # data = json.loads(response.text)
-
-
-        # hours = ["6AM","7AM","8AM", "9AM","10AM","11AM", "12PM","1PM","2PM", "3PM","4PM","5PM", "6PM","7PM","8PM","9PM","10PM","11PM", "12AM"]
-        # percent_fullness = data['analysis'][5]['day_raw'][:-5]
-        # print(percent_fullness)
-        # plotdata = [go.Bar(x = hours, y = percent_fullness)]
-        # fig = go.Figure(data=plotdata)
-        # fig.update_yaxes(range=[0, 100], color='lightgrey', showgrid=False)
-        # fig.update_layout(
-        #     autosize=False,
-        #     width=800,
-        #     height=400,
-        #     paper_bgcolor='rgba(0,0,0,0)',
-        #     plot_bgcolor='rgba(0,0,0,0)'
-        # )
-        # fig.update_layout(
-        #     xaxis = dict(
-        #         tickmode = 'array',
-        #         tickvals = ["9AM", "12PM", "3PM","6PM","9PM", "12AM"], showgrid=False, color='lightgrey'
-        #     )
-        # )
-        # # fig.show()
-        # fig.write_image("./static/images/crowdlevel" + str(counter) + ".png")
-        # crowdlevel_lists.append('images/crowdlevel' + str(counter) + ".png")
         counter += 1
 
-        if (counter == 2):
+        if (counter == 10):
             break
     length = counter
 
--- model.py
+++ model.py
@@ -12,6 +12,7 @@ new_features = pd.DataFrame()
 
 new_features['Latitude'] = features['Latitude'].astype(float)
 new_features['Longitude'] = features['Longitude'].astype(float)
+print(new_features.head())
 
 
 km = KMeans(n_clusters = 50, init = 'k-means++', max_iter = 300, random_state = 234)
@@ -24,15 +25,15 @@ pickle.dump(km, open("save.pkl", "wb"))
 
 latlong_df.to_csv("updatedlatlong.csv")
 
-# import plotly.graph_objects as go
-# import plotly_express as px
+import plotly.graph_objects as go
+import plotly_express as px
 
-# fig = px.scatter(latlong_df, x="Longitude", y="Latitude", color='Clusters')
+fig = px.scatter(latlong_df, x="Latitude", y="Longitude", color='Clusters')
 
-# fig.add_trace(go.Scatter(x=km.cluster_centers_[:,0], y=km.cluster_centers_[:, 1],
-#                     mode='markers',
-#                     name='Centroid', 
-#                     marker_color='rgba(0, 0, 0, .9)'))
+fig.add_trace(go.Scatter(x=km.cluster_centers_[:,0], y=km.cluster_centers_[:, 1],
+                    mode='markers',
+                    name='Centroid', 
+                    marker_color='rgba(0, 0, 0, .9)'))
 
-# fig.show()
+fig.show()
 
